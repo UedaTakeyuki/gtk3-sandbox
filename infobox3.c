7662bbc8b5f225149ca31e27a518463e71392a16
Dr. Takeyuki Ueda <gde00107@nifty.com>
2023-04-03 17:45:36 +0900

    not working

--- infobox3.c
+++ infobox3.c
@@ -1,4 +1,12 @@
 #include <gtk/gtk.h>
+#include <stdio.h>     // for fprintf()
+#include <unistd.h>    // for close()
+#include <sys/epoll.h> // for epoll_create1()
+#include <string.h>    // for strncmp
+#include <stdlib.h>
+#include <fcntl.h>
+#define MAX_EVENTS 5
+#define READ_SIZE 10
 
 GtkWidget* label;
 
@@ -17,6 +25,43 @@ activate (GtkApplication *app,
   gtk_container_add (GTK_CONTAINER (window), label);
 
   gtk_widget_show_all (window);
+}
+
+void* thread_function(void *data){
+  int epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1) {
+		fprintf(stderr, "Failed to create epoll file descriptor\n");
+	}
+
+	size_t bytes_read;
+	char read_buffer[READ_SIZE + 1];
+  int running = 1, event_count, i;
+
+  int fd = open("info", O_RDONLY);
+  struct epoll_event event, events[MAX_EVENTS];;
+  event.events = EPOLLIN;
+	event.data.fd = fd;
+
+  if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event)){
+		fprintf(stderr, "Failed to add file descriptor to epoll\n");
+		close(epoll_fd);
+	}
+
+  while(running){
+    printf("\nPolling for input...\n");
+		event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
+		printf("%d ready events\n", event_count);
+		for (int i = 0; i < event_count; i++) {
+			printf("Reading file descriptor '%d' -- ", events[i].data.fd);
+			bytes_read = read(events[i].data.fd, read_buffer, READ_SIZE);
+			printf("%zd bytes read.\n", bytes_read);
+			read_buffer[bytes_read] = '\0';
+			printf("Read '%s'\n", read_buffer);
+		
+			if(!strncmp(read_buffer, "stop\n", 5))
+			running = 0;
+		}
+  }
 }
 
 gboolean my_callback(GIOChannel *source, GIOCondition condition, gpointer data){
@@ -61,9 +106,10 @@ main (int    argc,
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);  
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 
-  channel = g_io_channel_new_file ("info", "r", NULL);
-  g_io_add_watch(channel, G_IO_IN | G_IO_HUP,(GIOFunc) my_callback, NULL);
-
+//  channel = g_io_channel_new_file ("info", "r", NULL);
+//  g_io_add_watch(channel, G_IO_IN | G_IO_HUP,(GIOFunc) my_callback, NULL);
+  GThread *thread = g_thread_new(NULL, thread_function, NULL);
+  
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
